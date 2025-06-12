aac5442ce94fef518f112d48dcaf172db58417ea
Samridhi Gumber <samridhigumber05@gmail.com>
2025-06-12 21:07:18 +0530

    Create simple_interest.cpp
    Output:
    1527.6

---
+++ basics/simple_interest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float p = 570;
+    float r = 134;
+    float t = 2;
+    cout<<"Simple interest will be: ";
+    cout<<(p*r*t) / 100;
+    
+    return 0;
+}
