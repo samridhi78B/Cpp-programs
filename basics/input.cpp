5c4fcbf73d941b6301c98f90bd393a417d7253d3
Samridhi Gumber <samridhigumber05@gmail.com>
2025-06-13 13:11:30 +0530

    Create input.cpp

--- basics/input.cpp
+++ basics/input.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main(){
     int x;
     float y;
-    char ch;
-    cout<<"Enter integer value: ";
-    cin>>x;
+    char ch; 
+    cout<<"Enter integer value: "; // cout for output as string
+    cin>>x; //cin for input
     cout<<"Enter float value: ";
     cin>>y;
     cout<<"Enter character: ";
     cin>>ch;
-    cout<<"Integer: ";
+    cout<<"Integer: "; //cout for output 
     cout<<x<<endl;
     cout<<"Float: ";
     cout<<y<<endl;
