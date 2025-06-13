bdd4df112648d67849b8b801da5ac54d6c6f0bb6
Samridhi Gumber <samridhigumber05@gmail.com>
2025-06-13 13:10:31 +0530

    Create input.cpp
    Output:
    Enter integer value: 4
    Enter float value: 1.2
    Enter character: a
    Integer: 4
    Float: 1.2
    Char: a

---
+++ basics/input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    float y;
+    char ch;
+    cout<<"Enter integer value: ";
+    cin>>x;
+    cout<<"Enter float value: ";
+    cin>>y;
+    cout<<"Enter character: ";
+    cin>>ch;
+    cout<<"Integer: ";
+    cout<<x<<endl;
+    cout<<"Float: ";
+    cout<<y<<endl;
+    cout<<"Char: ";
+    cout<<ch;
+   
+    return 0;
+}
