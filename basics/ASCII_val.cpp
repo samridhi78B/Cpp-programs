ca446cc0b00d142a97a6f9bac079b7d3ce450883
Samridhi Gumber <samridhigumber05@gmail.com>
2025-06-13 13:19:02 +0530

    Create ASCII_val.cpp
    Every character has its own ASCII value.For example, if input is a its output will be 97, if b,then,98 and A=65,B=67 and so on.

---
+++ basics/ASCII_val.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cin >> ch;               // Take integer input
+    int y = (int)ch;     // Explicitly cast ch to integer(ASCII value)
+    cout << y;              // Output the float value
+
+    return 0;
+}
