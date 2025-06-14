080ca77c9b4ac86f848b6dcfa2706fb3034aa7c1
Samridhi Gumber <samridhigumber05@gmail.com>
2025-06-14 20:29:45 +0530

    Create divisibility.cpp

---
+++ conditionals/divisibility.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    if(x % 3 == 0)
+{
+    cout<<"x is divisible by 3";
+}
+    else{
+        cout<<"x is not divisible by 3";
+    }
+}
