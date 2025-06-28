135f80a37edac077f4a28051229dcbe26d955bfa
Samridhi Gumber <samridhigumber05@gmail.com>
2025-06-28 18:52:16 +0530

    Update secondSmallest.cpp

--- array/secondSmallest.cpp
+++ array/secondSmallest.cpp
@@ -16,7 +16,7 @@ int main(){
      }
     }
      for(int i=0; i<n; i++){
-     if(smallest != secSmall && secSmall > arr[i]){
+     if(arr[i] != smallest && secSmall > arr[i]){
          secSmall= arr[i];
      }
     }
