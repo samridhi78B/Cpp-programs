36f4da6216ebf9200990d1234469d197147d89d5
Samridhi Gumber <samridhigumber05@gmail.com>
2025-06-28 18:46:24 +0530

    Create secondSmallest.cpp

---
+++ array/secondSmallest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    int smallest = INT_MAX;
+    int secSmall = INT_MAX;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+     for(int i=0; i<n; i++){
+     if(arr[i] < smallest){
+         smallest = arr[i];
+     }
+    }
+     for(int i=0; i<n; i++){
+     if(smallest != secSmall && secSmall > arr[i]){
+         secSmall= arr[i];
+     }
+    }
+    cout<<secSmall;
+    return 0;
+}
