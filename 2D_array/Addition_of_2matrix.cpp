9d23dcdf47e047a1a1e85f60b39ca8c3b0e2b31c
Samridhi Gumber <samridhigumber05@gmail.com>
2025-06-30 00:14:54 +0530

    Create Addition_of_2matrix.cpp

---
+++ 2D_array/Addition_of_2matrix.cpp
@@ -0,0 +1,33 @@
+include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    int r,c;
+    cin>>r>>c;
+    int arr[r][c];
+    int brr[r][c];
+    int sum[r][c];
+    for(int i=0; i<r;i++){
+        for(int j=0; j<c;j++){
+        cin>>arr[i][j];
+    }
+    }
+    for(int i=0; i<r;i++){
+        for(int j=0; j<c;j++){
+        cin>>brr[i][j];
+    }
+    }
+    
+    for(int i=0; i<r;i++){
+        for(int j=0; j<c;j++){
+        sum[i][j] = arr[i][j] + brr[i][j];
+    }
+    }
+    
+    for(int i=0; i<r;i++){
+        for(int j=0; j<c;j++){
+        cout<<sum[i][j]<<" ";
+    }
+    cout<<endl;
+    }
+}
