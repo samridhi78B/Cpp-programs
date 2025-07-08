5eb87e0620361d6af058f47ddc4f8519f0e202e5
Samridhi Gumber <samridhigumber05@gmail.com>
2025-07-09 00:55:19 +0530

    Create for_each_Loop.cpp

---
+++ Vectors/for_each_Loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector <int> vec = {1,2,3,4,5};
+   for(int i : vec){
+       cout<<i<<endl;
+   }
+    return 0;
+}
+
+
+
