102bb38e9ba9c2facbd292b96a2b3910bc7a6ca9
Samridhi Gumber <samridhigumber05@gmail.com>
2025-07-09 01:04:07 +0530

    Create functions.cpp

---
+++ Vectors/functions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector <int> vec;
+    vec.push_back(2);//insert
+    vec.push_back(4);//insert 
+    vec.push_back(8);//insert
+    vec.push_back(16); //insert 
+    vec.push_back(32); //insert at last
+    vec.pop_back(); //Delete last element
+    cout<<"AT first index: "<<vec.front()<<endl;
+     cout<<"AT last index: "<<vec.back()<<endl;
+     cout<<"Value at index: "<<vec.at(1)<<endl;
+    cout<<vec.size();
+    return 0;
+}
