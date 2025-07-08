f5d57269044676259a47d3d01e6c1620c28a84cb
Samridhi Gumber <samridhigumber05@gmail.com>
2025-07-09 00:53:54 +0530

    Create basics.cpp

---
+++ Vectors/basics.cpp
@@ -0,0 +1,16 @@
+//it will print 1 2 3 4 5
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector <int> vec = {1,2,3,4,5};
+    cout<<vec[0]<<" ";
+    cout<<vec[1]<<" ";
+    cout<<vec[2]<<" ";
+    cout<<vec[3]<<" ";
+    cout<<vec[4]<<" ";
+    return 0;
+}
+
+
+
