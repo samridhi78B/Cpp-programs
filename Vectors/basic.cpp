45b6c43943be5b00eeee22872daf1537fc4443f3
Samridhi Gumber <samridhigumber05@gmail.com>
2025-07-09 00:50:47 +0530

    Create basic.cpp

---
+++ Vectors/basic.cpp
@@ -0,0 +1,17 @@
+// It will print 0 0 0 0 0
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector <int> vec(5,0); //vector_namme(size,value at every index)
+    cout<<vec[0]<<" ";
+    cout<<vec[1]<<" ";
+    cout<<vec[2]<<" ";
+    cout<<vec[3]<<" ";
+    cout<<vec[4]<<" ";
+    return 0;
+}
+
+
+
