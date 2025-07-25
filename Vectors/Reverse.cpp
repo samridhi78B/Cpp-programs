dffc9d0dbe0eafa09e0b74782a8409c1f40df21d
Samridhi Gumber <samridhigumber05@gmail.com>
2025-07-25 11:51:04 +0530

    Update Reverse.cpp

--- Vectors/Reverse.cpp
+++ Vectors/Reverse.cpp
@@ -2,22 +2,24 @@
 #include <vector>
 using namespace std;
 int main(){
-    vector<int>vec = {2,3,1,4,5};
-    cout<<"Vector Before reversing: "<<endl;
-    for(int val : vec){
-   cout<<val<<" ";
-   }
-    int start=0, end= vec.size()-1;
-    while(start < end){
-        swap(vec[start],vec[end]);
-       start++;
-       end--;
-   }
-   cout<<endl;
-   cout<<"Vector after reversing: "<<endl;
-   for(int val : vec){
-   cout<<val<<" ";
-   }
+vector<int>vec;
+
+int n;
+cin>>n;
+for(int i =0; i<n;i++){
+    int x;
+    cin>>x;
+    vec.push_back(x);
+}
+int start=0,end=n-1;
+while(start < end){
+    swap(vec[start],vec[end]);
+    start++;
+    end--;
+}
+for(int val : vec){
+    cout<<val;
+}
     return 0;
 }
 
