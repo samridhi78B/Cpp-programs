c6c14c08c713132cb90abf554ae5951d136f1777
Samridhi Gumber <samridhigumber05@gmail.com>
2025-07-25 11:47:25 +0530

    Update linear_search.cpp\

--- Vectors/linear_search.cpp
+++ Vectors/linear_search.cpp
@@ -2,14 +2,24 @@
 #include <vector>
 using namespace std;
 int main(){
-    int target = 8;
-    vector <int> vec = {4,2,7,8,1,2,5};
-    for(int i = 0; i<vec.size();i++){
-        if(vec[i] == target){
-            cout<<i;
-            break;
-        }
+vector<int>vec;
+
+int n;
+cin>>n;
+for(int i =0; i<n;i++){
+    int x;
+    cin>>x;
+    vec.push_back(x);
+}
+bool found=false;
+int target=8;
+for(int i =0; i<n;i++){
+    if(vec[i] ==  target){
+        cout<<i;
+        found = true;
+        break;
     }
+}
+if(!found)cout<<"Not found";
     return 0;
 }
-
